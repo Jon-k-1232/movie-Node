faa275bee87f156144a2a9125df58eef65aee03d
Jon Kimmel <jonkimmel16@gmail.com>
2019-12-20 22:39:40 -0700

    commit 2 fix ignore

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const movies = require('./movieData.json');
-const cors = require('cors')
-const helmet = require('helmet')
+const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
 
 app.use(morgan('dev'));
@@ -14,8 +14,8 @@ app.use(cors());
 
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
@@ -43,7 +43,7 @@ app.get('/movie', function handleGetMovies(req,res){
 
     if (req.query.avg_vote) {
         response = response.filter(movie =>
-            Number(movie.avg_vote)) >= Number(req.avg_vote)
+            Number(movie.avg_vote)) >= Number(req.query.avg_vote)
     }
 
     res.json(response)
